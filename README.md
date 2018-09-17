805227ab924383aba3dd688e555c8c2033e6966a
KeithZhang <g.fengling.11@gmail.com>
2018-09-17 14:57:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# OH 命令行工具
+
+无论是 react 还是 react-native，无论 web，h5 还是 app，我们都想会新建项目，打包，部署等，所以 oh-cli 想统一规范一个方式，去创建项目，打包，部署。
+
+## cli
+
+## plugin
---
+++ packages/cli/bin/oh.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const program = require('commander');
+
+program.version('0.0.1').usage('<command> [options]');
+
+program
+  .command('build <app-name>')
+  .description('build all files of the given app by oh-cli')
+  .action((name, cmd) => {
+    console.log('name..', name);
+    console.log('cmd..', cmd);
+  });
+
+program
+  .command('create <app-name>')
+  .description('create a new project by oh-cli')
+  .option('-f, --force', 'Overwrite target derictory if it exists')
+  .action((name, cmd) => {
+    console.log('name..', name);
+    console.log('cmd..', cmd);
+  });
+
+program.on('--help', () => {
+  console.log('');
+  console.log(
+    `  Run ${chalk.cyan(
+      'oh <command> --help '
+    )} for detailed usage of given command `
+  );
+  console.log('');
+});
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
